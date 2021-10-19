23d5a132aed628df459a08008d0d8bc7f1ab2692
André Sartori <afs25@cam.ac.uk>
2021-10-19 13:38:21 +0100

    #4204 comment: Edited DeploymentEvent rule

--- deploy_to_aws.py
+++ deploy_to_aws.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-from thiscovery_dev_tools.deploy_to_aws import AwsDeployer
+from thiscovery_dev_tools.aws_deployer import AwsDeployer
 
 import local.dev_config  # set environment variables
 import local.secrets  # set environment variables
 from src.common.constants import STACK_NAME
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     deployer = AwsDeployer(stack_name=STACK_NAME)
     deployer.main()
